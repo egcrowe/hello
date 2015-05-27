879a157e21c196ec057fcda1161a1d0e9c0adc9b
Graham Crowe <graham.crowe@ericsson.com>
2015-05-27 13:39:18 +0200

    Fix hello boot file

--- src/hello.erl
+++ src/hello.erl
@@ -1,11 +1,32 @@
 -module(hello).
 
--export([start/0]).
+-behaviour(gen_server).
 
-start() ->
-  spawn(fun() -> loop(0) end).
+-export([start_link/0]).
 
-loop(N) ->
-  ok = timer:sleep(10000),
+-export([init/1,
+         terminate/2,
+         code_change/3,
+         handle_call/3,
+         handle_cast/2,
+         handle_info/2]).
+
+-define(PERIOD, 10000).
+
+start_link() ->
+  gen_server:start_link(?MODULE, [], []).
+
+init([]) ->
+  {ok, 0, ?PERIOD}.
+
+handle_call(_, _, N) -> {noreply, N}.
+
+handle_cast(_, N) -> {noreply, N}.
+
+handle_info(timeout, N) ->
   ok = error_logger:info_msg("Hello~n"),
-  loop(N + 1).
+  {noreply, N + 1, ?PERIOD}.
+
+code_change(_, N, _) -> {ok, N}.
+
+terminate(_, _) -> ok.
